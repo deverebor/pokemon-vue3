d22e1a468e6e1f8af72390781d5f799abac04ab4
Lucas Souza <lucasp.sdev@gmail.com>
2022-01-07 20:31:08 -0300

    fix: deploy path

--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,3 @@
 module.exports = {
-  publicPath: process.env.NODE_ENV === 'production'
-    ? '/pokemon-vue3/'
-    : '/'
-}
+  publicPath: process.env.NODE_ENV === "production" ? "/pokemon-vue3/" : "/",
+};
